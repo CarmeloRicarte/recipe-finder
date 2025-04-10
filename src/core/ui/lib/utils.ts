a56b56bc090b410b045173d31ce4bf323756f04e
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2025-04-10 18:49:00 +0200

    feat(utils): add sanitizeHTML function to remove dangerous tags

--- src/core/ui/lib/utils.ts
+++ src/core/ui/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+/**
+ * Sanitizes HTML string by removing potentially dangerous tags and their contents
+ * @param html - The HTML string to sanitize
+ * @returns The sanitized HTML string with dangerous tags removed
+ */
+export function sanitizeHTML(html: string) {
+	return html
+		.replace(/<script[^>]*>([\S\s]*?)<\/script>/gim, "")
+		.replace(
+			/<\/?(script|iframe|frame|object|embed|style|link|meta)[^>]*>/gim,
+			"",
+		);
+}
