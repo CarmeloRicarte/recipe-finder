5d1c32a2a534c690a2b9af1a981508bb56d254c8
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2025-04-11 13:46:26 +0200

    test(GetRandomRecipesUseCase): correct the number of recipes to fetch

--- src/recipe/application/useCases/__tests__/GetRandomRecipesUseCase.test.ts
+++ src/recipe/application/useCases/__tests__/GetRandomRecipesUseCase.test.ts
@@ -1,35 +1,33 @@
-import { GetRandomRecipesUseCase } from '../GetRandomRecipesUseCase';
-import type { RecipeRepository } from '@/recipe/domain/repositories';
-import { mockRecipes } from '@/recipe/__mocks__/recipesMock';
+import { mockRecipes } from "@/recipe/__mocks__/recipesMock";
+import type { RecipeRepository } from "@/recipe/domain/repositories";
+import { GetRandomRecipesUseCase } from "../GetRandomRecipesUseCase";
 
-describe('GetRandomRecipesUseCase', () => {
-  it('should get random recipes', async () => {
-    
-    const mockRecipeRepository: RecipeRepository = {
-      getRandomRecipes: vi.fn().mockResolvedValue(mockRecipes),
-      getRecipeById: vi.fn(),
-      searchRecipes: vi.fn(),
-    };
-    const useCase = new GetRandomRecipesUseCase(mockRecipeRepository);
+describe("GetRandomRecipesUseCase", () => {
+	it("should get random recipes", async () => {
+		const mockRecipeRepository: RecipeRepository = {
+			getRandomRecipes: vi.fn().mockResolvedValue(mockRecipes),
+			getRecipeById: vi.fn(),
+			searchRecipes: vi.fn(),
+		};
+		const useCase = new GetRandomRecipesUseCase(mockRecipeRepository);
 
-    const result = await useCase.execute();
+		const result = await useCase.execute();
 
-    expect(mockRecipeRepository.getRandomRecipes).toHaveBeenCalledWith(10);
-    expect(result).toEqual(mockRecipes);
-  });
+		expect(mockRecipeRepository.getRandomRecipes).toHaveBeenCalledWith(10);
+		expect(result).toEqual(mockRecipes);
+	});
 
-  it('should get random recipes with specified number', async () => {
-    
-    const mockRecipeRepository: RecipeRepository = {
-      getRandomRecipes: vi.fn().mockResolvedValue(mockRecipes),
-      getRecipeById: vi.fn(),
-      searchRecipes: vi.fn(),
-    };
-    const useCase = new GetRandomRecipesUseCase(mockRecipeRepository);
+	it("should get random recipes with specified number", async () => {
+		const mockRecipeRepository: RecipeRepository = {
+			getRandomRecipes: vi.fn().mockResolvedValue(mockRecipes),
+			getRecipeById: vi.fn(),
+			searchRecipes: vi.fn(),
+		};
+		const useCase = new GetRandomRecipesUseCase(mockRecipeRepository);
 
-    const result = await useCase.execute(2);
+		const result = await useCase.execute(2);
 
-    expect(mockRecipeRepository.getRandomRecipes).toHaveBeenCalledWith(4);
-    expect(result).toEqual(mockRecipes);
-  });
+		expect(mockRecipeRepository.getRandomRecipes).toHaveBeenCalledWith(2);
+		expect(result).toEqual(mockRecipes);
+	});
 });
