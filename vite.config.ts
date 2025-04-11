35ab5c63c9c162dbfca6e6c3a1172080fe3aab80
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2025-04-11 13:45:42 +0200

    style(vite.config.ts): format file

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,41 @@
 /// <reference types="vitest/config" />
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'node:path';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "node:path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      include: ['src'],
-      reporter: ['text', 'json', 'html'],
-      exclude: [
-        'src/main.tsx',
-        'src/vite-env.d.ts',
-        'src/**/index.ts',
-        'src/**/__mocks__/*',
-        'src/**/__tests__/*',
-        'src/App.tsx',
-        'src/core/ui/**/*.tsx',
-      ],
-      thresholds: {
-        branches: 80,
-        functions: 80,
-        statements: 80,
-        lines: 80,
-      },
-    },
-    restoreMocks: true,
-    setupFiles: ['./vitest.setup.ts'],
-  },
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		coverage: {
+			include: ["src"],
+			reporter: ["text", "json", "html"],
+			exclude: [
+				"src/main.tsx",
+				"src/vite-env.d.ts",
+				"src/**/index.ts",
+				"src/**/__mocks__/*",
+				"src/**/__tests__/*",
+				"src/App.tsx",
+				"src/core/ui/**/*.tsx",
+			],
+			thresholds: {
+				branches: 80,
+				functions: 80,
+				statements: 80,
+				lines: 80,
+			},
+		},
+		restoreMocks: true,
+		setupFiles: ["./vitest.setup.ts"],
+	},
 });
