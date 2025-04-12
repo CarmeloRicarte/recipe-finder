14064c152552abb6438477af7088b70fb2a76697
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2025-04-12 12:15:58 +0200

    test: add mock IntersectionObserver for testing

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,9 +1,21 @@
-import * as matchers from '@testing-library/jest-dom/matchers';
-import { cleanup } from '@testing-library/react';
-import { afterEach, expect } from 'vitest';
+import * as matchers from "@testing-library/jest-dom/matchers";
+import { cleanup } from "@testing-library/react";
+import { afterEach, expect, vi } from "vitest";
 
 expect.extend(matchers);
 
 afterEach(() => {
-  cleanup();
+	cleanup();
 });
+
+const mockObserver = vi.fn((callback, options) => ({
+	root: null,
+	rootMargin: "0px",
+	thresholds: [0],
+	takeRecords: () => [],
+	unobserve: vi.fn(),
+	disconnect: vi.fn(),
+	observe: vi.fn(),
+}));
+
+global.IntersectionObserver = mockObserver;
